8a17fde66fb4e86bc802df9e8ae4e3ff75b659f0
josh.uni99@gmail.com <josh.uni99@gmail.com>
2026-05-11 22:12:39 -0500

    Fix/Correccion error app.tsx y css root

--- coreSystems/src/App.tsx
+++ coreSystems/src/App.tsx
@@ -1,18 +1,21 @@
 // src/App.tsx
 
-import React, { useState } from 'react';
-import Home from './pages/Home/Home';
-import LoginPage from './pages/Login/LoginPage';
+import React, { useState } from "react";
+
+import Home from "./pages/Home/Home";
+import LoginPage from "./pages/Login/LoginPage";
 
 const App: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const handleAuthSuccess = () => {
+  const handleAuthSuccess = (): void => {
     setIsAuthenticated(true);
   };
 
   if (!isAuthenticated) {
-    return <LoginPage onSuccess={handleAuthSuccess} />;
+    return (
+      <LoginPage onSuccess={handleAuthSuccess} />
+    );
   }
 
   return <Home />;
